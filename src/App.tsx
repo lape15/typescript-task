f638762d5d20a50cceeec7423015f6f41791c595
lape <lape@formpl.us>
2021-11-28 13:30:24 +0100

    Fixed more console warnings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
-import { Header } from './layout/header';
+// import { Header } from './layout/header';
 import { SideMenu } from './layout/side-menu';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Pages } from './pages';
 import GlobalContextProvider from './context';
 
